196fb8ddadb993462c23ab586e9d3c8d0e4dac88
tchakra2 <TathagataChakraborti@users.noreply.github.com>
2018-04-29 17:13:29 -0700

    home page done

---
+++ home/files/js/custom.js
@@ -0,0 +1,21 @@
+/********************************
+Custom scripts for Yochan Homepage
+********************************/
+
+// slow-scroll on active anchor tags
+
+$(function() {
+$('a[href*="#"]:not([href="#"])').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+var target = $(this.hash);
+target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+if (target.length) {
+$('html, body').animate({
+scrollTop: target.offset().top
+}, 1000);
+return false;
+}
+}
+});
+});
+
